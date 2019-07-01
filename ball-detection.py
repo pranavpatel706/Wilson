02635caba0a13c5c90c4fa1d70ca9ee84c999d15
pranavpatel706 <51907417+pranavpatel706@users.noreply.github.com>
2019-07-01 19:38:38 +0530

    Add files via upload

---
+++ ball-detection.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+cap.set(3, 640)
+cap.set(4, 480)
+kernel = np.ones((10, 10), np.uint8)
+while True:
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_yellow = np.array([9, 47, 215])
+    upper_yellow = np.array([31, 255, 255])
+    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    contours, _ = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        area = cv2.contourArea(c)
+        approx = cv2.approxPolyDP(c, 0.01*cv2.arcLength(c, True), True)
+        #cv2.drawContours(frame, [approx], 0, (0, 0, 0), 5)
+        if area > 400:
+            if 15 < len(approx) :
+                # calculate moments for each contour
+                M = cv2.moments(c)
+                # calculate x,y coordinate of center
+                if M["m00"] != 0:
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                    cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
+                    cv2.putText(frame, "centroid", (cX - 25, cY - 25), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 3)
+                    cv2.drawContours(frame, [approx], 0, (0, 0, 0), 5)
+
+
+    # display the image
+    cv2.imshow("frame", frame)
+    cv2.imshow("openong", opening)
+    key = cv2.waitKey(10)
+    if key == 27:
+       break
+cap.release()
+cv2.destroyAllWindows()
---
+++ third.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+cap = cv2.VideoCapture(-1)
+cv2.namedWindow("tracking")
+cv2.createTrackbar("lh", "tracking", 0, 255,nothing)
+cv2.createTrackbar("ls", "tracking", 0, 255,nothing)
+cv2.createTrackbar("lv", "tracking", 0, 255,nothing)
+cv2.createTrackbar("uh", "tracking", 255, 255,nothing)
+cv2.createTrackbar("us", "tracking", 255, 255,nothing)
+cv2.createTrackbar("uv", "tracking", 255, 255,nothing)
+kernel = np.ones((5, 5), np.uint8)
+
+while True:
+      _, frame = cap.read()
+      hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+      lh = cv2.getTrackbarPos("lh", "tracking")
+      ls = cv2.getTrackbarPos("ls", "tracking")
+      lv = cv2.getTrackbarPos("lv", "tracking")
+      uh = cv2.getTrackbarPos("uh", "tracking")
+      us = cv2.getTrackbarPos("us", "tracking")
+      uv = cv2.getTrackbarPos("uv", "tracking")
+
+      l_b = np.array([lh, ls, lv])
+      u_b = np.array([uh, us, uv])
+
+      mask = cv2.inRange(hsv,l_b,u_b)
+      opening = cv2.erode(mask, kernel)
+
+      contours, _ = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+      #for cnt in contours:
+            #cv2.drawContours(frame, [cnt], 0, (0,0,0),5)
+      res = cv2.bitwise_and(frame,frame,mask=mask)
+
+      cv2.imshow("frame", frame)
+      cv2.imshow("mask", mask)
+      cv2.imshow("res", res)
+
+      key = cv2.waitKey(1)
+      if key == 27:
+          break
+cap.release()
+cv2.destroyAllWindows()
